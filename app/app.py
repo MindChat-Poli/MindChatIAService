eef7c402e3b38091fd7b05ff73902342d2a0b140
DoseiBrayin <ardilabrayan68@gmail.com>
2023-10-31 13:18:55 -0500

    (delete) delete modules dont used

--- app/app.py
+++ app/app.py
@@ -1,16 +1,6 @@
 from flask import Flask, jsonify
-from injector import inject, Injector, singleton
-from domain.user import User
-from application.user_repository import UserService, InMemoryUserRepository
-
-# Set up the Injector instance
-def configure(binder):
-    binder.bind(UserService, to=UserService, scope=singleton)
-
-injector = Injector([configure])
 
 app = Flask(__name__)
-app.config['INJECTOR'] = injector
 
 @app.route('/ping')
 def ping():
